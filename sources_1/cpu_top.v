9c2355fd2a46f3f054b3b8c4b56cb810559e3070
ybcybcybc1 <2402471397@qq.com>
2025-03-25 12:55:56 +0800

    Delete sources_1 directory

--- sources_1/cpu_top.v
+++
@@ -1,8 +0,0 @@
-//****************************
-//	top module of cpu core
-//****************************
-`include	"D:\Grade2\RISC_V\CPU\CPU.srcs\sources_1\config.v"
-module cpu_top(
-
-    );
-endmodule
